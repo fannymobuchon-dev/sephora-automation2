1107debd4c1e8b8ffb4cdc5b47b47a468482a179
fannymobuchon-dev <fanny.mobuchon@gmail.com>
2026-03-03 11:51:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Utiliser Browserless pour les captures
 const BROWSERLESS_TOKEN = process.env.BROWSERLESS_TOKEN || 'YOUR_BROWSERLESS_TOKEN';
